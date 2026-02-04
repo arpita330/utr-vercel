568439bac8a4912711d602e9d2b329cc69774db0
arpita330 <arpitanaha721@gmail.com>
2026-02-05 00:42:02 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,13 @@
 <script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
 
 <script>
-
 firebase.initializeApp({
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_PROJECT.firebaseapp.com",
-databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-projectId: "YOUR_PROJECT"
+apiKey: "AIzaSyA1mqCS0DPdgVnzi04Leyhk0E018vblTik",
+authDomain: "upi-pay-6fc37.firebaseapp.com",
+databaseURL: "https://upi-pay-6fc37-default-rtdb.asia-southeast1.firebasedatabase.app",
+projectId: "upi-pay-6fc37",
+storageBucket: "upi-pay-6fc37.firebasestorage.app",
+messagingSenderId: "1063195602640",
+appId: "1:1063195602640:web:c7e4ca62d5f734b95fb8d5"
 })
-
 </script>
