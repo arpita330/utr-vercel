f40ab5a3e1987fcda09fc3c9597f9fa27993b5b3
arpita330 <arpitanaha721@gmail.com>
2026-02-05 00:13:26 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
+
+<script>
+
+firebase.initializeApp({
+apiKey: "YOUR_API_KEY",
+authDomain: "YOUR_PROJECT.firebaseapp.com",
+databaseURL: "https://YOUR_PROJECT.firebaseio.com",
+projectId: "YOUR_PROJECT"
+})
+
+</script>
